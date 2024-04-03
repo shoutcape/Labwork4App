943b85e70849840cb9a5fd4f3c2e9906e2491921
Ville Kautiainen <kautiainen.ville@gmail.com>
2024-04-03 10:15:55 +0300

    Update Firebase config with environment variables

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -4,13 +4,13 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 
 const config = {
-    apiKey: "AIzaSyDb2aXIyr37goo8usdN9B8_kz55GdokxZY",
-    authDomain: "labwork-4-92b09.firebaseapp.com",
-    databaseURL: "https://labwork-4-92b09.firebaseio.com",
-    projectId: "labwork-4-92b09",
-    storageBucket: "labwork-4-92b09.appspot.com",
-    messagingSenderId: "220986087366",
-    appId: "1:220986087366:web:02f675f5f41416f85404c7"
+    apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_REACT_APP_DATABASE_URL,
+    projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_APP_ID
 };
 
 firebase.initializeApp(config)
